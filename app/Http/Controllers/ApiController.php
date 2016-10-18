4f0b6718e77302bc18285c183b6802d44bc689e8
Brandon Lotriet <lotrietb@gmail.com>
2016-10-18 14:40:22 -0700

    Added the format parameter and added json as the default Content-Type

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,38 +7,46 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Vehicle;
 use Illuminate\Support\Facades\Validator;
+use SoapBox\Formatter\Formatter;
 
 class ApiController extends Controller
 {
-	public function vehicles() {
-		return Vehicle::all();
+	private $response_format = 'json';
+
+	public function __construct(Request $request){
+		$format = $request->get('format');
+		$this->response_format = (isset($format) && in_array($format, ['json', 'xml']))? $format: 'json';
 	}
 
-	public function vehicle($id) 
+	public function vehicles() {
+		$this->generate_response(Vehicle::all()->toArray());
+	}
+
+	public function vehicle($id)
     {    	
     	$vehicle = Vehicle::find($id);
 
     	if (!empty($vehicle)) {
-    		return $vehicle;
+    		$this->generate_response($vehicle->toArray());
     	} else {
-    		return [
+    		$this->generate_response([
     			'status' => 'error',
     			'code' => 'VEHICLE_NOT_FOUND'
-    		];
+    		]);
     	}
     }
 
     public function add($id) 
     {    	
-    	$vehicle = Vehicle::find($id);
+    	$vehicle = Vehicle::find($id)->toArray();
 
     	if (!empty($vehicle)) {
-    		return $vehicle;
+    		$this->generate_response($vehicle->toArray());
     	} else {
-    		return [
+    		$this->generate_response([
     			'status' => 'error',
     			'code' => 'VEHICLE_NOT_FOUND'
-    		];
+    		]);
     	}
     }
 
@@ -46,7 +54,7 @@ class ApiController extends Controller
     {    	
     	$validation_results = $this->validate_fields($request->all());
 
-    	if($validation_results !== true) return $validation_results;
+    	if($validation_results !== true) $this->generate_response($validation_results);
 
     	$vehicle = Vehicle::find($id);
     	if (!empty($vehicle)) {
@@ -61,15 +69,15 @@ class ApiController extends Controller
 		    $vehicle->mileage = $request->mileage;
 		    $vehicle->save();
 
-		    return [
+		    $this->generate_response([
     			'status' => 'success',
     			'code' => 'VEHICLE_UPDATED'
-    		];
+    		]);
     	} else {
-    		return [
+    		$this->generate_response([
     			'status' => 'error',
     			'code' => 'VEHICLE_NOT_FOUND'
-    		];
+    		]);
     	}
     }
 
@@ -77,7 +85,7 @@ class ApiController extends Controller
     public function save(Request $request){
     	$validation_results = $this->validate_fields($request->all());
 
-    	if($validation_results !== true) return $validation_results;
+    	if($validation_results !== true) $this->generate_response($validation_results);
 
     	$vehicle = new Vehicle([
     		'owner_name' => $request->name,
@@ -92,10 +100,10 @@ class ApiController extends Controller
     	]);
     	
     	$vehicle->save();
-    	return [
-    			'status' => 'success',
-    			'code' => 'VEHICLE_ADDED'
-    		];
+    	$this->generate_response([
+			'status' => 'success',
+			'code' => 'VEHICLE_ADDED'
+    	]);
     }
 
     public function remove($id) 
@@ -104,15 +112,15 @@ class ApiController extends Controller
     	if (!empty($vehicle)) {
 	    	$vehicle->delete();
 
-		    return [
+		    $this->generate_response([
     			'status' => 'success',
     			'code' => 'VEHICLE_DELETED'
-    		];
+    		]);
     	} else {
-    		return [
+    		$this->generate_response([
     			'status' => 'error',
     			'code' => 'VEHICLE_NOT_FOUND'
-    		];
+    		]);
     	}
     }
 
@@ -140,5 +148,17 @@ class ApiController extends Controller
     	}
 
     	return true;
+    }
+
+    private function generate_response($resp_value) {
+    	if ($this->response_format == 'json') {
+    		header('Content-Type: application/json');
+			echo json_encode($resp_value);
+    	} else {
+    		$xml = Formatter::make($resp_value, Formatter::XML);
+    		header('Content-Type: application/xml');
+    		echo $xml->toXml();
+    	}
+    	exit();
     }
 }
